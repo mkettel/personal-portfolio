ce8f15c243baf42089618b06268a6b87f4688766
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-06-28 14:51:14 -0400

    good enough for now

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -19,19 +19,19 @@ export default function Navbar({page, setPage}) {
       <h1>Matthew Kettelkamp</h1>
     </div>
     <div className="navbar-container nav">
-      <div onClick={() => {setPage('about'), setActive('tile-about')}} id='tile-about' className={`nav-item ${active === 'tile-about' ? 'active' : ''} blue-grey`}>
+      <div onClick={() => {setPage('about'), setActive('tile-about')}} id='tile-about' className={`nav-item ${active === 'tile-about' ? 'active' : 'none'} blue-grey`}>
         <a className="nav-link" >about</a>
       </div>
-      <div onClick={() => {setPage('skills'), setActive('tile-skills')}} id='tile-skills' className={`nav-item ${active === 'tile-skills' ? 'active' : ''} tiffany-blue`} >
+      <div onClick={() => {setPage('skills'), setActive('tile-skills')}} id='tile-skills' className={`nav-item ${active === 'tile-skills' ? 'active' : 'none'} tiffany-blue`} >
         <a className='nav-link' >skills</a>
       </div>
-      <div onClick={() => setPage('projects')} id='tile-project' className="nav-item purple">
+      <div onClick={() => {setPage('projects'), setActive('tile-projects')}} id='tile-projects' className={`nav-item ${active === 'tile-projects' ? 'active' : 'none'} purple`}>
         <a className="nav-link" >projects</a>
       </div>
-      <div onClick={() => setPage('contact')} id='tile-contact' className="nav-item chinese-violet">
+      <div onClick={() => setPage('contact')} id='tile-contact' className="nav-item none chinese-violet">
         <a className="nav-link" >contact</a>
       </div>
-      <div onClick={() => setPage('three')} id='tile-3d' className="nav-item old-rose">
+      <div onClick={() => setPage('three')} id='tile-3d' className="nav-item none old-rose">
         <a className="nav-link" >3D</a>
       </div>
     </div>
