c45b9c78506206342d40a1a8630a955007d5f39a
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-06-28 22:13:06 -0400

    final adds to push

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -18,8 +18,8 @@ export default function Navbar({page, setPage}) {
     <div className="header-container grey">
       <h1>Matthew Kettelkamp</h1>
       <div className="link-box">
-        <a href="#">Github</a>
-        <a href="#">LinkedIn</a>
+        <a href="https://github.com/mkettel" target='_blank'>Github</a>
+        <a href="https://www.linkedin.com/in/matthew-kettelkamp-100490b2/" target='_blank'>LinkedIn</a>
         <a href="#">Email</a>
       </div>
     </div>
@@ -27,11 +27,11 @@ export default function Navbar({page, setPage}) {
       <div onClick={() => {setPage('about'), setActive('tile-about')}} id='tile-about' className={`nav-item ${active === 'tile-about' ? 'active' : 'none'} blue-grey`}>
         <a className="nav-link" >about</a>
       </div>
-      <div onClick={() => {setPage('skills'), setActive('tile-skills')}} id='tile-skills' className={`nav-item ${active === 'tile-skills' ? 'active' : 'none'} tiffany-blue`} >
-        <a className='nav-link' >skills</a>
-      </div>
       <div onClick={() => {setPage('projects'), setActive('tile-projects')}} id='tile-projects' className={`nav-item ${active === 'tile-projects' ? 'active' : 'none'} purple`}>
         <a className="nav-link" >projects</a>
+      </div>
+      <div onClick={() => {setPage('skills'), setActive('tile-skills')}} id='tile-skills' className={`nav-item ${active === 'tile-skills' ? 'active' : 'none'} tiffany-blue`} >
+        <a className='nav-link' >skills</a>
       </div>
       <div onClick={() => {setPage('contact'), setActive('tile-contact')}} id='tile-contact' className={`nav-item ${active === 'tile-contact' ? 'active' : 'none'} chinese-violet`}>
         <a className="nav-link" >contact</a>
