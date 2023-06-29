d994c046f66346b56978f2b3bb17d33e37936087
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-06-29 16:53:55 -0400

    addd the project element and need to design the card now for it

---
+++ src/components/ProjectComponent.jsx
@@ -0,0 +1,22 @@
+
+
+export default function ProjectComponent() {
+
+
+  const projectArray = [
+    {
+      id: 1,
+      title: 'Surfshop'
+    }
+  ]
+
+  return<>
+
+      <div className="project-container">
+        <div className="project-card">
+          <h3>Project 1</h3>
+        </div>
+      </div>
+
+  </>
+}
--- src/three/ImageCubes.jsx
+++ src/three/ImageCubes.jsx
@@ -31,8 +31,8 @@ export default function ImageCubes() {
       function handleResize() {
         const { innerWidth } = window;
         const isMobile = innerWidth <= 768; // Adjust the breakpoint for mobile devices
-        const scale = isMobile ? .9 : 1.6; // Adjust the scale values for mobile
-        const position = isMobile ? [0, 0, 0] : [0, 0, 0]
+        const scale = isMobile ? 1 : 1.6; // Adjust the scale values for mobile
+        const position = isMobile ? [0, 0, -.5] : [0, 0, 0]
         setImageScale(scale);
         setImagePosition(position);
       }
