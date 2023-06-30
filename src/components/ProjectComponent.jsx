06ea4f4bc28d2b937b86f1a78118fe2315d21c7c
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-06-30 17:11:09 -0400

    added links and jolt video

--- src/components/ProjectComponent.jsx
+++ src/components/ProjectComponent.jsx
@@ -8,19 +8,29 @@ export default function ProjectComponent() {
       id: 1,
       title: 'Surfshop',
       videoUrl: '/videos/surfshop-vid.mp4',
-      lang: ['React', 'React Three Fiber', 'Vercel']
+      lang: ['React', 'React Three Fiber', 'Blender'],
+      link: "https://surf-shop-zeta.vercel.app/"
     },
     {
       id: 2,
       title: 'Homebase',
       videoUrl: '/videos/homebase.mp4',
-      lang: ['React', 'React Three Fiber', 'Vercel', 'GSAP']
+      lang: ['React', 'React Three Fiber', 'GSAP', 'Blender'],
+      link: "https://homebase-mkettel.vercel.app/"
     },
     {
       id: 3,
       title: '3D Resume',
       videoUrl: '/videos/3d-resume-vid.mp4',
-      lang: ['React', 'React Three Fiber', 'Vercel']
+      lang: ['React', 'React Three Fiber', 'Vercel'],
+      link: "https://3d-resume-six.vercel.app/"
+    },
+    {
+      id: 4,
+      title: 'Jolt Chrome Extension',
+      videoUrl: '/videos/jolt-recording.mp4',
+      lang: ['React', 'Node.js', 'Firebase', 'GSAP'],
+      link: "#"
     },
 
 
@@ -36,6 +46,11 @@ export default function ProjectComponent() {
             <div key={project.id} className="project-card">
               <div className="project-title">
                 <h2>{project.title}</h2>
+              </div>
+              <div className="project-link-link">
+                <a target="_blank" href={project.link}>
+                  <i className="fa-solid fa-arrow-up-right-from-square"></i>
+                </a>
               </div>
               <div className="screen-image">
                 <video className="video-player" playsInline autoPlay muted loop src={project.videoUrl}></video>
