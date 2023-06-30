9cdd8044c6d55927f7daa8ac867947c10a3745c1
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-06-30 16:02:50 -0400

    added some videos and the card to the project page

--- src/components/ProjectComponent.jsx
+++ src/components/ProjectComponent.jsx
@@ -6,16 +6,51 @@ export default function ProjectComponent() {
   const projectArray = [
     {
       id: 1,
-      title: 'Surfshop'
-    }
-  ]
+      title: 'Surfshop',
+      videoUrl: '/videos/surfshop-vid.mp4',
+      lang: ['React', 'React Three Fiber', 'Vercel']
+    },
+    {
+      id: 2,
+      title: 'Homebase',
+      videoUrl: '/videos/homebase.mp4',
+      lang: ['React', 'React Three Fiber', 'Vercel', 'GSAP']
+    },
+    {
+      id: 3,
+      title: '3D Resume',
+      videoUrl: '/videos/3d-resume-vid.mp4',
+      lang: ['React', 'React Three Fiber', 'Vercel']
+    },
 
+
+  ]
+  console.log(projectArray.map((project) => {
+    project
+  }));
   return<>
 
       <div className="project-container">
-        <div className="project-card">
-          <h3>{projectArray[0].title}</h3>
-        </div>
+        {projectArray.map((project) => {
+          return (
+            <div key={project.id} className="project-card">
+              <div className="project-title">
+                <h2>{project.title}</h2>
+              </div>
+              <div className="screen-image">
+                <video className="video-player" autoPlay muted loop src={project.videoUrl}></video>
+              </div>
+              <div className="screen-overlay"></div>
+              <div className="project-lang">
+              {project.lang.map((lang) => {
+                return (
+                  <p>{lang}</p>
+                )
+              })}
+              </div>
+            </div>
+          )
+        })}
       </div>
 
   </>
