ff33a971809ee552377e027d96290d3ecba7c920
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-07-03 18:49:25 -0400

    added the gh links

--- src/components/ProjectComponent.jsx
+++ src/components/ProjectComponent.jsx
@@ -9,41 +9,48 @@ export default function ProjectComponent() {
       title: 'Surfshop',
       videoUrl: '/videos/surfshop-vid.mp4',
       lang: ['React', 'React Three Fiber', 'Blender'],
-      link: "https://surf-shop-zeta.vercel.app/"
+      link: "https://surf-shop-zeta.vercel.app/",
+      gh: 'https://github.com/mkettel/SurfShop'
     },
     {
       id: 2,
       title: 'Homebase',
       videoUrl: '/videos/homebase.mp4',
       lang: ['React', 'React Three Fiber', 'GSAP', 'Blender'],
-      link: "https://homebase-mkettel.vercel.app/"
+      link: "https://homebase-mkettel.vercel.app/",
+      gh: 'https://github.com/mkettel/HomeBase'
     },
     {
       id: 3,
       title: '3D Resume',
       videoUrl: '/videos/3d-resume-vid.mp4',
       lang: ['React', 'React Three Fiber', 'Vercel'],
-      link: "https://3d-resume-six.vercel.app/"
+      link: "https://3d-resume-six.vercel.app/",
+      gh: 'https://github.com/mkettel/3d-resume'
     },
     {
       id: 4,
       title: 'Jolt',
       videoUrl: '/videos/jolt-recording.mp4',
-      lang: ['React', 'Node.js', 'Firebase', 'GSAP']
+      lang: ['React', 'Node.js', 'Firebase', 'GSAP'],
+      gh: 'https://github.com/mkettel/Jolt'
     },
     {
       id: 5,
       title: 'Deepwork.',
       videoUrl: '/videos/deepwork.mp4',
       lang: ['Ruby', 'Rails', 'SASS', 'JS'],
-      link: "https://deepwork-official.herokuapp.com/"
+      link: "https://deepwork-official.herokuapp.com/",
+      gh: 'https://github.com/SkiltonJ/Deepwork.'
+
     },
     {
       id: 6,
       title: 'AVTECHDADDY',
       videoUrl: '/videos/techdaddy-video.mp4',
       lang: ['HTML', 'CSS', 'JS'],
-      link: "https://av-techdaddy.vercel.app/"
+      link: "https://av-techdaddy.vercel.app/",
+      gh: 'https://github.com/mkettel/AV-TECHDADDY'
     },
 
 
@@ -65,8 +72,16 @@ export default function ProjectComponent() {
                   <a target="_blank" href={project.link}>
                     <i className="fa-solid fa-arrow-up-right-from-square"></i>
                   </a>
+                  <a target="_blank" href={project.gh}>
+                  <i class="fa-brands fa-github"></i>
+                  </a>
                 </div>
-              ) : ( <div className="project-link-link"></div> )}
+              ) : (
+              <div className="project-link-link">
+                <a target="_blank" href={project.gh}>
+                  <i class="fa-brands fa-github"></i>
+                  </a>
+              </div> )}
               <div className="screen-image">
                 <video className="video-player" playsInline autoPlay muted loop src={project.videoUrl}></video>
               </div>
--- src/three/ImageCubes.jsx
+++ src/three/ImageCubes.jsx
@@ -45,6 +45,11 @@ export default function ImageCubes() {
     };
   }, []);
 
+  useFrame((state, delta) => {
+    groupRef.current.rotation.y += .01 * delta;
+    groupRef.current.rotation.x += .01 * delta;
+  })
+
   // Hover state for cube
   const [hovered, setHovered] = useState(false);
 
