4c30c21584f4afd91078f7df4e7d964a7386465f
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-07-25 14:18:43 -0400

    added autorotate to the imagecube on the about page

--- src/components/AboutImage.jsx
+++ src/components/AboutImage.jsx
@@ -17,7 +17,7 @@ export default function AboutImage() {
       <div className='small-canvas'>
         <Canvas>
           <PerspectiveCamera position={[0, 0, 0]} />
-          <OrbitControls />
+          <OrbitControls autoRotate autoRotateSpeed={.5} />
           <ambientLight intensity={.5} />
 
           <Suspense fallback={<Placeholder scale={1.5} position={[0, 0, 0]} rotation={[0, .6, 0]} />}>
