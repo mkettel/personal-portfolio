9a4136e2a1b25f7da0c2fd34c11ba9830b8925a1
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-08-01 19:10:12 -0400

    tried moving the cube down just a little bit on mobile

--- src/three/ImageCubes.jsx
+++ src/three/ImageCubes.jsx
@@ -17,7 +17,7 @@ export default function ImageCubes() {
         const { innerWidth } = window;
         const isMobile = innerWidth <= 768; // Adjust the breakpoint for mobile devices
         const scale = isMobile ? 1 : 1.6; // Adjust the scale values for mobile
-        const position = isMobile ? [0, 0, -.5] : [0, 0, 0]
+        const position = isMobile ? [0, -.5, -.5] : [0, 0, 0]
         setImageScale(scale);
         setImagePosition(position);
       }
